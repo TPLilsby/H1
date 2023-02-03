bfabab7d06e5c79439cde6429cf0e4c2b0320c34
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-02-03 13:23:26 +0100

    Friday the 13th

---
+++ Friday 13th/Friday 13th/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    for (int j = 1; j <= 13; j++)
+    {
+    DateTime date = new DateTime(DateTime.Today.Year + 1, j, 13);
+
+        if (date.DayOfWeek == DayOfWeek.Friday)
+        {
+            Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " Is a friday");
+        }
+    }
+*/
+
+int year = 2023;
+for (int y = 0; y <= 10; y++)
+{
+    for (int m = 1; m <= 12; m++)
+    {
+        DateTime date = new DateTime(year, m, 13);
+
+        if (date.DayOfWeek == DayOfWeek.Friday)
+        {
+            Console.WriteLine(date.ToString("dd\\t\\h MMMM yyyy") + " Is a friday");
+        }
+    }
+    year++;
+}

4cdb2a73c194e65b96826eaaa9dce466914aa40e
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-02-06 09:33:09 +0100

    Superhero

---
+++ Superheltene/Superheltene/Superhero.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheltene
+{
+    public class Superhero
+    {
+        private string _identity;
+        private string _ability;
+        private string _equipment;
+        private string _costume;
+        private string _moralCode;
+        private string _enemy;
+        private string _anxiety;
+
+        public string Identity
+        { 
+            get { return _identity; }
+
+            private set { _identity = value; }
+        }
+
+        public string Ability 
+        { 
+            get { return _ability; } 
+            
+            private set { _ability = value;  } 
+        }
+
+        public string Equipment
+        {
+            get { return _equipment; }
+
+            private set { _equipment = value; }
+        }
+
+        public string Costume
+        {
+            get { return _costume; }
+
+            private set { _costume = value; }
+        }
+
+        public string MoralCode
+        {
+            get { return _moralCode; }
+
+            private set { _moralCode = value; }
+        }
+
+        public string Enemy
+        {
+            get { return _enemy; }
+
+            private set { _enemy = value; }
+        }
+
+        public string Anxiety
+        {
+            get { return _anxiety; }
+
+            private set { _anxiety = value; }
+        }
+    }
+}

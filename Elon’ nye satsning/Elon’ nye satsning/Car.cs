c014aa30da740bc32fbc9a0d5e01702866f72493
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-02-06 13:17:24 +0100

    Elon's nye satsning

---
+++ Elon’ nye satsning/Elon’ nye satsning/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elon__nye_satsning
+{
+    public class Car
+    {
+        private string _colour;
+        private byte _batteryCapacity = 100;
+        private int _distance = 0;
+
+        public string Colour
+        {
+            get { return _colour; }
+
+            set { _colour = value; }
+        }
+
+        public byte BatteryCapacity
+        {
+            get { return _batteryCapacity;}
+            
+            set { _batteryCapacity = value; }
+        }
+
+        public int Distance
+        {
+            get { return _distance;}
+
+            set { _distance = value; }
+        }
+
+        public void Drive()
+        {
+            Thread.Sleep(200);
+            _batteryCapacity--;
+            _distance += 20;
+
+        }
+    }
+}
---
+++ Elon’ nye satsning/Elon’ nye satsning/Program.cs
@@ -0,0 +1,44 @@
+﻿using Elon__nye_satsning;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Car car1 = new Car();
+
+
+        car.Colour = "Blå";
+        string carColour = car.Colour;
+
+        car1.Colour = "Rød";
+        string car1Volour = car1.Colour;
+
+        Console.WriteLine(CarDriving(car));
+        Console.WriteLine(CarDriving(car1));
+
+    }
+
+    public static string CarDriving(Car c)
+    {
+        bool notStop = true;
+        string message = "";
+
+        while (notStop)
+        {
+            if (c.BatteryCapacity == 0)
+            {
+                notStop = false;
+                message = "Kan ikke kører mere";
+                
+            }
+            Console.WriteLine("Pres a key: ");
+            Console.ReadKey();
+
+            c.Drive();
+            Console.WriteLine("Den " + c.Colour + " bil har kørt: " + c.Distance + " meter og har: " + c.BatteryCapacity + "% tilbage");
+        }
+        return message;
+    }
+}

7cf418fbc6b6b01cff39a19e030dcef4be95942d
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-02-08 09:15:26 +0100

    Fordelingshatten

---
+++ Fordelingshatten/Fordelingshatten/Program.cs
@@ -0,0 +1,34 @@
+﻿public class SortingHat
+{
+    public enum Colleges { Gryffindor, Hufflepuff, Ravenclaw, Slytherin }
+
+    public static void Main(string[] args)
+    {
+        SortingHat hat = new SortingHat();
+
+        Console.WriteLine(hat.Sort());
+    }
+
+    public Colleges Sort()
+    {
+            Random random = new Random();
+            int number = random.Next(1, 5);
+
+            if (number == 1)
+            {
+                return Colleges.Gryffindor;
+            }
+            else if (number == 2)
+            {
+                return Colleges.Hufflepuff;
+            }
+            else if (number == 3)
+            {
+                return Colleges.Ravenclaw;
+            }
+            else
+            {
+                return Colleges.Slytherin;
+            }
+    }
+}

305844a21b7df2e24eb398cb3fbbfafafaa01615
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-02-09 13:18:25 +0100

    En tur på biblioteket

---
+++ En tur på biblioteket/En tur på biblioteket/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace En_tur_på_biblioteket
+{
+    public class Book
+    {
+        //Defining varibles
+        private byte _id;
+        private string _name;
+        private string _genre;
+        private string _author;
+        private int _pages;
+        private int _releaseYear;
+
+        //Incapsulating
+        public byte Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Genre { get { return _genre; } set { _genre = value; } }
+        public string Author { get { return _author; } set { _author = value; } }
+        public int Pages { get { return _pages; } set { _pages = value; } }
+        public int ReleaseYear { get { return _releaseYear; } set { _releaseYear = value; } }
+
+        //Book Constructor
+        public Book(byte id, string name, string genre, string author, int pages, int releaseYear)
+        {
+            Id = id;
+            Name = name;
+            Genre = genre;
+            Author = author;
+            Pages = pages;
+            ReleaseYear = releaseYear;
+        }
+    }
+}
---
+++ En tur på biblioteket/En tur på biblioteket/Program.cs
@@ -0,0 +1,135 @@
+﻿using En_tur_på_biblioteket;
+using System.Collections.Generic;
+
+/// <summary>
+/// Libary program where you can rent books
+/// </summary>
+
+//Program class
+public class Program
+{
+    //Main method
+    public static void Main(string[] args)
+    {
+        //Entered data for books
+        Book harryPotter1 = new Book(1, "Harry Potter and the Philosopher's Stone", "Fantasy", "J.K. Rowling", 223, 1997);
+        Book theHobbit = new Book(2, "The Hobbit", "Fantasy", "J.R.R. Tolkien", 310, 1937);
+        Book lordOfTheRings1 = new Book(3, "The Fellowship of the Ring", "Fantasy", "J.R.R. Tolkien", 423, 1957);
+
+        //List with all the books in
+        List<Book> lendingList = new List<Book>();
+
+        //Add the books
+        lendingList.Add(harryPotter1);
+        lendingList.Add(theHobbit);
+        lendingList.Add(lordOfTheRings1);
+        
+        //A stack to the user loans
+        Stack<Book> userLoan = new Stack<Book>();
+
+        //Bool to the first while loop
+        bool pickBooks = true;
+
+        //First while loop | pick books from the libary
+        while (pickBooks)
+        {
+            //Print all the books
+            foreach (Book book in lendingList)
+            {
+                Console.WriteLine(book.Id + " | " + book.Name + " | " + book.Genre + " | " + book.Author + " | " + book.Pages + " | " + book.ReleaseYear);
+            }
+
+            //User input's ID to pick the book they want to rent
+            Console.Write("\nEnter the ID on the book you want to rent or (quit/q: ");
+            char bookId = char.Parse(Console.ReadLine());
+
+            //Switch to the diffrent options
+            switch (bookId)
+            {
+                //The book with ID 1
+                case '1':
+
+                    //Removes the book from the list
+                    lendingList.Remove(harryPotter1);
+
+                    //Add the book to the stack
+                    userLoan.Push(harryPotter1);
+                    break;
+
+                //The book with ID 2
+                case '2':
+
+                    //Removes the book from the list
+                    lendingList.Remove(theHobbit);
+
+                    //Add the book to the stack
+                    userLoan.Push(theHobbit);
+                    break;
+
+                //The book with ID 3
+                case '3':
+
+                    //Removes the book from the list
+                    lendingList.Remove(lordOfTheRings1);
+
+                    //Add the book to the stack
+                    userLoan.Push(lordOfTheRings1);
+                    break;
+
+                //Quits the while loop when user don't want to rent more books
+                case 'q':
+                    pickBooks = false;
+                    break;
+            }
+        }
+
+        //Clear the console
+        Console.Clear();
+
+        //count the books in the stack to see how many is back
+        byte countBooks = 0;
+        foreach (Book book in userLoan)
+        {
+            countBooks++;
+        }
+        
+        //Bool to the second while loop
+        bool rentBooks = true;
+
+        //Second while loop | rent books from the user stack
+        while (rentBooks)
+        {
+            //If there is no books back in the stack, print the writeline & set rentbooks to false
+            if (countBooks == 0)
+            {
+                Console.WriteLine("\nNo more books to rent:");
+                rentBooks = false;
+            }
+            //Rent books
+            else
+            {
+                //Clear console
+                Console.Clear();
+
+                //looks what the next book in the stack is and print it
+                Book book = userLoan.Peek();
+                Console.WriteLine(book.Name + " | " + book.Genre + " | " + book.Author + " | " + book.Pages + " | " + book.ReleaseYear);
+
+                //Asks user to press (r) to rent
+                Console.Write("Rent book: press (r): ");
+                char choice = char.Parse(Console.ReadLine());
+
+                //If choice is (r) it removes the book from the stack and minus countBooks with 1
+                if (choice == 'r')
+                {
+                    //Remove from stack
+                    userLoan.Pop();
+
+                    //Minus countBooks with 1
+                    countBooks--;
+                }
+            }
+        }
+        
+    }
+}

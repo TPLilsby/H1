2f959f831a74fde9a2dfa1aa03bb0b041949d468
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-06-21 08:37:17 +0200

    Portofolio

--- Portofolio hjemmeside/Portofolio hjemmesideCSharp/Portofolio hjemmesideCSharp/DALManager.cs
+++ Portofolio hjemmeside/Portofolio hjemmesideCSharp/Portofolio hjemmesideCSharp/DALManager.cs
@@ -7,133 +7,185 @@ using System.Diagnostics;
 
 namespace Portofolio_hjemmesideCSharp
 {
+    //DALManager class, get things from the database
     public class DALManager
     {
+        //Private readonly IConfiguration instance used for accessing configuration settings
         private readonly IConfiguration Configuration;
 
+        //Constructor for DALManager class
         public DALManager(IConfiguration configuration)
         {
+            //Assign the provided IConfiguration instance to the Configuration property
             Configuration = configuration;
         }
 
+        //Return the fetched list of Cv objects
         public List<Cv> GetCv()
         {
-            List<Cv> cv = new List<Cv>();
+            //Create a new list to store the fetched Cv objects
+            List<Cv> cvList = new List<Cv>();
 
+            //Create a new NpgsqlConnection using the specified connectionString from the appsettings
             using (NpgsqlConnection connection = new NpgsqlConnection(Configuration["ConnectionString"]))
             {
+                //Try statement
                 try
                 {
+                    //Makes a new NpgSglCommand named command
                     using (NpgsqlCommand command = new NpgsqlCommand())
                     {
+                        //Set the command's connection and query for get_cv(1) sql function
                         command.Connection = connection;
                         command.CommandText = "SELECT * FROM get_cv(1)";
 
+                        //Open the database connection
                         connection.Open();
 
+                        //Execute the command and retrieve the results using a NpgsqlDataReader
                         using (NpgsqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                cv.Add(new Cv(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4)));
+                                //Create a new Cv object and add it to the cvList,  with the diffrent columns in the database
+                                cvList.Add(new Cv(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4)));
                             }
                         }
                     }
                 }
+                //Catch database exceptions
                 catch (NpgsqlException npg)
                 {
+                    //Print it to the debug console
                     Debug.Write("22222222222222222222222222222222222" + npg.Message);
                 }
+                //Catch all other exceptions
                 catch (Exception ex)
                 {
+                    //Print it to the debug console
                     Debug.Write("An error occurred: " + ex.Message);
                 }
                 finally
                 {
+                    //Close the database connection in the finally block
                     connection.Close();
                 }
             }
-            return cv;
+
+            //Return the fetched list of Cv objects
+            return cvList;
         }
 
+         //Return the fetched list of Project objects
         public List<Project> GetProject()
         {
+            //Create a new list to store the fetched Project objects
             List<Project> projectList = new List<Project>();
 
+            //Create a new NpgsqlConnection using the specified connectionString from the appsettings
             using (NpgsqlConnection connection = new NpgsqlConnection(Configuration["ConnectionString"]))
             {
+                //Try statement
                 try
                 {
+                    //Makes a new NpgSglCommand named command
                     using (NpgsqlCommand command = new NpgsqlCommand())
                     {
+                        //Set the command's connection and query for getproject() sql function
                         command.Connection = connection;
                         command.CommandText = "SELECT * FROM getproject()";
 
+                        //Open the database connection
                         connection.Open();
 
+                        //Execute the command and retrieve the results using a NpgsqlDataReader
                         using (NpgsqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
+                                //Create a new Project object and add it to the projectList,  with the diffrent columns in the database
                                 projectList.Add(new Project(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4)));
                             }
                         }
                      }
                 }
+                //Catch database exceptions
                 catch (NpgsqlException npg)
                 {
+                    //Print it to the debug console
                     Debug.Write("22222222222222222222222222222222222" + npg.Message);
                 }
+                //Catch all other exceptions
                 catch (Exception ex)
                 {
+                    //Print it to the debug console
                     Debug.Write("An error occurred: " + ex.Message);
                 }
                 finally
                 {
+                    //Close the database connection in the finally block
                     connection.Close();
                 }
             }
+
+            //Return the fetched list of Project objects
             return projectList;
         }
 
+        //Method named GetTechnology that return a list Technology objects
         public List<Technology> GetTechnology()
         {
-            List<Technology> projectList = new List<Technology>();
+            //Create a new list to store the fetched Technology objects
+            List<Technology> tecknologyList = new List<Technology>();
 
+            //Create a new NpgsqlConnection using the specified connectionString from the appsettings
             using (NpgsqlConnection connection = new NpgsqlConnection(Configuration["ConnectionString"]))
             {
+                //Try statement
                 try
                 {
+                    //Makes a new NpgSglCommand named command
                     using (NpgsqlCommand command = new NpgsqlCommand())
                     {
+                        //Set the command's connection and query for gettechnology() sql function
                         command.Connection = connection;
                         command.CommandText = "SELECT * FROM gettechnology()";
 
+                        //Open the database connection
                         connection.Open();
 
+                        //Execute the command and retrieve the results using a NpgsqlDataReader
                         using (NpgsqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                projectList.Add(new Technology(reader.GetInt32(0), reader.GetString(1), reader.GetString(2)));
+                                //Create a new Technology object and add it to the technologyList,  with the diffrent columns in the database
+                                tecknologyList.Add(new Technology(reader.GetInt32(0), reader.GetString(1), reader.GetString(2)));
                             }
                         }
                     }
                 }
+                //Catch database exceptions
                 catch (NpgsqlException npg)
                 {
+                    //Print it to the debug console
                     Debug.Write("22222222222222222222222222222222222" + npg.Message);
                 }
+                //Catch all other exceptions
                 catch (Exception ex)
                 {
+                    //Print it to the debug console
                     Debug.Write("An error occurred: " + ex.Message);
                 }
                 finally
                 {
+                    //Close the database connection in the finally block
                     connection.Close();
                 }
             }
-            return projectList;
+
+            //Return the fetched list of Technology objects
+            return tecknologyList;
         }
     }
 }
--- Portofolio hjemmeside/Porttofolio hjemmesideHTML/script.js
+++ Portofolio hjemmeside/Porttofolio hjemmesideHTML/script.js
@@ -1,12 +1,24 @@
 //Cv function
 $(document).ready(function() {
+   
+    //Send an AJAX request to retrieve CV data from the specified URL: API Endpoint
     $.ajax({
         url: "https://localhost:7073/GetCv"
     }).then(function(data) {
+   
+        //Loops through the received data and populate the corresponding elements in the HTML
         for (i = 0; i < data.length; i++) {
+
+            //Append the CV title to elements with the class "cv-title"
             $('.cv-title').append(data[i].title);
+            
+            //Append the CV description to elements with the class "cv-description"
             $('.cv-description').append(data[i].description);
-            $('#cv-image').attr("src", data[i].imagePath)
+            
+            //Set the source of the image element with the ID "cv-image" to the CV image path
+            $('#cv-image').attr("src", data[i].imagePath);
+            
+            //Set the href of the element with the ID "pdf-link" to the CV PDF link
             $('#pdf-link').attr("href", data[i].pdfLink);
         }
     });
@@ -14,53 +26,86 @@ $(document).ready(function() {
 
 //Technology function
 $(document).ready(function() {
+
+    //Send an AJAX request to retrieve CV data from the specified URL: API Endpoint
     $.ajax({
         url: "https://localhost:7073/GetTechnology"
     }).then(function(data) {
+
+        //Loops through the received data and dynamically generate HTML elements
         for (i = 0; i < data.length; i++) {
-            $('#technology').append('<div class="container"> <div class="row"> <div class="col-4"><h4 class="pt-4">' + data[i].name+ '</h4></div> <div class="col-4"> <img class="logo-resize" src="' + data[i].logoPath + '"></div> </div> </div>');
+
+            //Append a container div with a row and two columns to the element with the ID "technology"
+            $('#technology').append('<div class="container"> <div class="row"> <div class="col-6"><h4 class="pt-4">' + data[i].name+ '</h4></div> <div class="col-6"> <img class="logo-resize" src="' + data[i].logoPath + '"></div> </div> </div>');
         }
     });
 });
 
 //Project function
 $(document).ready(function() {
+    
+    //Send an AJAX request to retrieve CV data from the specified URL: API Endpoint
     $.ajax({
         url: "https://localhost:7073/GetProject"
     }).then(function(data) {
+
+        //Loops through the received data and dynamically generate HTML elements
         for (i = 0; i < data.length; i++) {
-            $('#project').append('<h1 class="pt-4 text-resize">' + data[i].title + '</h1>');
-            $('#project').append('<h4 text-resize>' + data[i].description + '</h4>');
+
+            // Append project title
+            $('#project').append('<div class="center text-resize"><h1 class="pt-4">' + data[i].title + '</h1></div>');
+            
+            // Append project description
+            $('#project').append('<h4 class="text-resize center">' + data[i].description + '</h4>');
+            
+            // Append project image element with the ID "project" to the Project image path
             $('#project').append('<img class="resize" src="' + data[i].imagePath + '">');
+            
+            // Append GitHub link with button element with the ID "project" to the GitHub link
             $('#project').append('<br><a target="_blank" href="' + data[i].githubLink + '"><button class="button m-lg-4">GitHub Link</button></a>');
         }
     });
 });
 
-// Contact function
-$(document).ready(function () {
-    $("#contactform").submit(function (event) {
-      var formData = {
-        FirstName: $("#fname").val(),
-        LastName: $("#lname").val(),
-        Email: $("#email").val(),
-        Text: $("#text").val()
-      };
-  
-      $.ajax({
-        type: "POST",
-        url: "https://localhost:7073/SubmitContact",
-        data: JSON.stringify(formData),
-        contentType: "application/json; charset=utf-8",
-        }).done (function (data) {
+//Contact function
+$(document).ready(function() {
+
+    //Triggered when the contact form is submitted
+    $("#contactform").submit(function(event) {
+
+        //Retrieve form data
+        var formData = {
+            FirstName: $("#fname").val(),
+            LastName: $("#lname").val(),
+            Email: $("#email").val(),
+            Text: $("#text").val()
+        };
+
+        //Send form data via AJAX POST request
+        $.ajax({
+            
+            //POST
+            type: "POST",
+
+            //API Endpoint
+            url: "https://localhost:7073/SubmitContact",
+
+            //Convert formData to JSON
+            data: JSON.stringify(formData),
+
+            //Specific JSON
+            contentType: "application/json; charset=utf-8",
+
+        }).done(function(data) {
+            //Sucess calback function
+            console.log(data); 
+
+        }).fail(function(data) {
+            //Error callback function
             console.log(data);
-        })
-        .fail (function (data) {
-          
-            console.log(data)
-          
-        })
-  
-      event.preventDefault();
+        });
+
+        //Prevent the default form submission behavior
+        event.preventDefault();
     });
-  });
+});
